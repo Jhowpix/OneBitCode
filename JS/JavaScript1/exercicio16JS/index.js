6977daa0cc31c3eeb1771dfe68f6ff74740af4f1
Jhonatan de Oliveira <Jhonatandevbr@gmail.com>
2023-03-10 08:56:52 +0900

    ajustes de arquivos
    ...

--- JS/JavaScript1/exercicio16JS/index.js
+++ JS/JavaScript1/exercicio16JS/index.js
@@ -1,87 +1,33 @@
+// minha segunda tentaiva de fazer sozinho nao deu certo
+// vamos refazer seguindo a aula
 
-let menus = 0;
-let candidato = '';
+const vagas = [];
 
-do{
-   
- 
-    function menu(){
-       menus = parseInt(prompt('SISTEMA DE VAGAS ' + 
-                               '\n\n1. Listar vagas ' +
-                               '\n2. Criar nova vaga ' +
-                               '\n3. Visualizar vaga ' +
-                               '\n4. Inscrever candidato ' +
-                               '\n5. Excluir vaga. ' +
-                               '\n6. Sair'))
+function listarVagas(){   
+                                     //funcao criada somente para formatar o texto
+    const vagasEmTexto = vagas.reduce( function(textoFinal, vagas, indice){
+        // texto final mostrara nome da vaga e quantidades de candidatos
+
+        textoFinal += indice + ". "
+        textoFinal += vagas.nome
+        textoFinal += " (" + vagas.candidatos.legth + "candidatos)\n"
+        return textoFinal
+    }, "")
+    alert(vagasEmTexto)
+}
+
+function novaVaga(){
+    const nome = prompt('Informe um nome para a vaga.')
+    const descricao = prompt('Informe uma descricao para a vaga.')
+    const dataLimite = prompt('Informe data limite desta vaga.')
+
+    const consirmacao = confirm('Deseja criar uma nova vaga?\n'+
+                                 'Nome: ' + nome + 
+                                 '\nDescricao: ' + descricao +
+                                 '\nData Limite: ' + dataLimite
+    )
+
+    if (consirmacao){
+        const novaVaga = {nome, descricao, dataLimite}
     }
-
-    function listarVagas(){
-         for(let i = 0; i < vagas.length; i++){
-           vagas += [i] + 1
-           alert(vagas)
-        }
-    }  
-
-    function visualizar(){
-       let verVaga = parseInt(prompt('insira o numero da vaga que gostaria de ver.'))
-    }
-
-    let vagas = [];
-
-    function criarVaga(){
-     let nomeVaga = prompt('insira nome da vaga');
-     let descVaga = prompt('descricao da vaga.');
-     let numeroVaga = prompt('Numero de vagas.');
-        vagas.push(
-           'Nome da vaga: ' + nomeVaga,
-           'Descricao: ' + descVaga,
-           'numero de vagas: ' + numeroVaga
-        )
-     console.log(vagas)
-     return vagas
-     
-    }
-
-    let vaga = {};
-
-    function inscrever(){
-     let candidato = prompt('Insira nome do candidato.');
-     let numeroDaVaga = parseInt(prompt('insira o numero da vaga.'))
-      vaga += candidato += vagas[numeroDaVaga]
-     return candidato
-    }
-
-    function excluir(){
-     let number = parseInt(prompt('Digite numero do candidato a ser excluido'))
-     candidato.push([number]);
-    }
-
-    switch(menus){
-        case 1:
-            listarVagas()
-            break
-            case 2:
-                criarVaga()
-                break
-                case 3:
-                    visualizar()
-                    break
-                    case 4:
-                        inscrever()
-                        break
-                        case 5:
-                            excluir()
-                            break
-                            case 6:
-                                alert('Saindo do sistema de vagas...');
-                                break
-                                default:
-                                    alert('Opcao invalida!');
-    }
-
-  
-    menu()
-    console.log(menus)
-    
-}while(menus !== 6 )
-
+}
